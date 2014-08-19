2ef9909235979c12e119c6265c5f47db99529f09
uzigula <uzigula@gmail.com>
2014-08-19 15:46:08 -0500

    Showing using ViewModels

--- Models/MVC5Course.Models/Controllers/AlbumsController.cs
+++ Models/MVC5Course.Models/Controllers/AlbumsController.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Linq;
-using System.Web;
+
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using MVC5Course.Models.DataModel;
 using MVC5Course.Models.DataModel.Context;
+using MVC5Course.Models.Models;
 
 namespace MVC5Course.Models.Controllers
 {
@@ -14,8 +16,25 @@ namespace MVC5Course.Models.Controllers
         ChinookContext _db = new ChinookContext();
         public ActionResult Index()
         {
-            var albums = _db.Albums.ToList();
+            var albums = _db.Artists.ToList();
             return View(albums);
+        }
+
+        public ActionResult MusicBy(Artist artist )
+        {
+            var songs = (from track in _db.PlaylistTracks
+                where track.Track.Album.ArtistId == artist.ArtistId
+                orderby track.Track.AlbumId
+                select new MusicByArtist
+                { 
+                    Artist = track.Track.Album.Artist.Name,
+                    Album =  track.Track.Album.Title,
+                    Genre = track.Track.Genre.Name,
+                    Title = track.Track.Name,
+                    Track = track.Track.TrackId
+                }).DistinctBy(x=>x.Title);
+
+            return View(songs);
         }
 	}
 }
---
+++ Models/MVC5Course.Models/Models/MusicByArtist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC5Course.Models.Models
+{
+    public class MusicByArtist
+    {
+        public String Artist { get; set; }
+        public String Album { get; set; }
+        public int Track { get; set; }
+        public String Title { get; set; }
+        public String Genre { get; set; }
+    }
+
+
+}
